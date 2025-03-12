59fbc4ee9b1d205729097381c7bc1517fbb31d68
J0V1K <mr.arifov@gmail.com>
2025-03-11 23:54:06 -0700

    Stays under 1000 words!

--- agent.py
+++ agent.py
@@ -1,9 +1,13 @@
 import os
 from mistralai import Mistral
 import discord
+import logging
+
 
 MISTRAL_MODEL = "mistral-large-latest"
 SYSTEM_PROMPT = "You are Peter Griffin who is acting as a helpful assistant."
+logger = logging.getLogger("discord")
+
 
 
 class MistralAgent:
@@ -17,15 +21,29 @@ class MistralAgent:
         # The first message is the system prompt
         messages = [{"role": "system", "content": SYSTEM_PROMPT}]
         # Set up chat history
+        total_words = len(message.content.split())
         for author_message_pair in history:
+            history_message_word_count = len(author_message_pair[1].content.split())
+            # check if we can fit this history message to prompt, if not skip rest of history
+            if total_words + history_message_word_count > 1000:
+                logger.info("Reached max word count of 1000, stopping history")
+                break
+            # set up right role and message for API call
             if author_message_pair[0] == "user":
                 messages.append({"role": "user", "content": author_message_pair[1].content})
             else:
                 messages.append({"role": "assistant", "content": author_message_pair[1].content})
+            # update word count currently
+            total_words += len(author_message_pair[1].content.split())
         
         # Add the newest message (question to model) to the chat history
-        messages.append({"role": "user", "content": message.content})
 
+        # Check if the message is too long
+        messages.append({"role": "user", "content": message.content})
+        if len(message.content.split()) > 1000:
+            logger.info("Reached max word count of 1000, stopping history")
+            return "I'm sorry, I can't answer that question. It's too long. Can you try again?"
+        
         # Get the response from the Mistral agent
         response = await self.client.chat.complete_async(
             model=MISTRAL_MODEL,
