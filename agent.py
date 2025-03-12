96e621d24a56189a11900f6e2a7aad311321dc3c
J0V1K <mr.arifov@gmail.com>
2025-03-12 00:15:48 -0700

    History in opposite order

--- agent.py
+++ agent.py
@@ -23,7 +23,7 @@ class MistralAgent:
         messages = [{"role": "system", "content": SYSTEM_PROMPT}]
         # Set up chat history
         total_words = len(message.content.split())
-        for author_message_pair in history:
+        for author_message_pair in history.reverse():
             history_message_word_count = len(author_message_pair[1].content.split())
             # check if we can fit this history message to prompt, if not skip rest of history
             if total_words + history_message_word_count > 1000:
